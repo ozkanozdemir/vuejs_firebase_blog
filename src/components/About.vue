608efa428be59226393124947feb3a290a8aa57c
ozkan <developer.ozkan@gmail.com>
2017-10-28 08:25:35 +0300

    added about and contact page contents

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    About
+    <h1>{{about.title}}</h1>
+    <p>{{about.content}}</p>
   </div>
 </template>
+
+<script>
+  export default {
+    data()
+    {
+      return { about : {}}
+    },
+    created() {
+      let _this = this
+      firebase.database().ref('/about/').once('value').then(function(snapshot) {
+        _this.about = snapshot.val()
+      });
+
+    }
+  }
+</script>
--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
-    Contact
+    <h1>Contact</h1>
+    <p>
+      {{ contact.content }}
+    </p>
+    <p>
+      @facebook: {{ contact.facebook }}
+    </p>
   </div>
 </template>
+
+<script>
+  export default {
+    data()
+    {
+      return { contact : {}}
+    },
+    created() {
+      let _this = this
+      firebase.database().ref('/contact/').once('value').then(function(snapshot) {
+        _this.contact = snapshot.val()
+      });
+
+    }
+  }
+</script>
